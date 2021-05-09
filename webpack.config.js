3a0d65faba64ea0b1fd0bf9c2cb65e931289528a
Gorjan Petrovski <mogi57@gmail.com>
2021-05-09 19:12:14 +0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/view/main/main.tsx",
+  entry: "./src/main.tsx",
   devtool: "inline-source-map",
   module: {
     rules: [
